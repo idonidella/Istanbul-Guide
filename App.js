67762fe7fee0e7c768fe71d38ae6a8b9a7c6f7f3
Efekan Efe <efekanefe63186@gmail.com>
2025-03-01 15:38:53 +0300

    initialize main application structure and update dependencies

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { StatusBar, View } from 'react-native';
+import Navigator from './src/router/index';
+
+class App extends Component {
+  
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar hidden />
+        <Navigator />
+      </View>
+    );
+  }
+}
+
+export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
 };
