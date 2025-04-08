b7b0620273366c18298f6011ece2c82d9df5463b
Efekan Efe <efekanefe63186@gmail.com>
2025-04-09 00:30:55 +0300

    fix: update comment for baseURL in AxiosInstance.js for clarity

--- src/networking/AxiosInstance.js
+++ src/networking/AxiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://192.168.1.147:3000',
+  baseURL: 'http://192.168.1.147:3000', 
+  //kendi internetiniz ip adresinizi buraya yazın
 });
 
 export default AxiosInstance;
