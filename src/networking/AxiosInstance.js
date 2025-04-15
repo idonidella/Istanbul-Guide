65598f30c617c8561c9c3a3b728d930988747b1f
Efekan Efe <efekanefe63186@gmail.com>
2025-04-15 23:41:54 +0300

    fix: update Axios baseURL to use the correct local IP address

--- src/networking/AxiosInstance.js
+++ src/networking/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://192.168.1.147:3000', 
+  baseURL: 'http://10.0.2.2:3000', 
   //kendi internetiniz ip adresinizi buraya yazın
 });
 
